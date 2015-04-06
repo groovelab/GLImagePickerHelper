b57e222422ea10531e1bb3fc5223a88e76fb01a7
nanba.takeo <nanba.takeo@linecorp.com>
2015-04-07 01:52:08 +0900

    delete Pods

--- Project/Pods/Target Support Files/Pods-GLImagePickerHelper-GLImagePickerHelper/Pods-GLImagePickerHelper-GLImagePickerHelper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GLImagePickerHelper_GLImagePickerHelper : NSObject
-@end
-@implementation PodsDummy_Pods_GLImagePickerHelper_GLImagePickerHelper
-@end
